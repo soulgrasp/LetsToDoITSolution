371c830c8c2a697a8717ad271d609431fb42b118
Niklas Bengtsson <de02beni@gmail.com>
2024-08-22 12:28:06 +0200

    Added Todo Note model

---
+++ LetsToDoIT/Models/Notes/Note.cs
@@ -0,0 +1,21 @@
+﻿namespace LetsToDoIT.Models.Notes
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        // Title
+        public string Title { get; set; }
+
+        // IsCompleted
+        public bool IsCompleted { get; set; }
+
+        // Timestamp
+        public DateTime Created { get; set; }
+
+        // Author
+        public string AuthorName {  get; set; }
+        
+
+    }
+}
